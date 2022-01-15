f9c25b6cef30f06cfb83a0797d4427d3efa16d32
jd_786 <jaydeep_shil@certree.com>
2022-01-15 11:20:37 +0530

    improved

--- Time Based Key-Value Store/Program.cs
+++ Time Based Key-Value Store/Program.cs
@@ -1,56 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 
-namespace Time_Based_Key_Value_Store
+public class TimeMap
 {
-  class Program
+  private readonly IDictionary<string, IList<(int timestamp, string value)>> _data;
+
+  public TimeMap()
   {
-    static void Main(string[] args)
-    {
-      Console.WriteLine("Hello World!");
-    }
+    _data = new Dictionary<string, IList<(int timestamp, string value)>>();
   }
 
-  public class TimeMap
+  public void Set(string key, string value, int timestamp)
   {
-    public Dictionary<string, Dictionary<int, string>> hash = null;
-    public TimeMap()
+    if (!_data.ContainsKey(key))
     {
-      hash = new Dictionary<string, Dictionary<int, string>>();
+      _data[key] = new List<(int timestamp, string value)>();
     }
 
-    public void Set(string key, string value, int timestamp)
+    _data[key].Add((timestamp, value));
+  }
+
+  public string Get(string key, int timestamp)
+  {
+    if (!_data.ContainsKey(key))
     {
-      if (hash.ContainsKey(key))
-      {
-        var existing = hash[key];
-        existing.Add(timestamp, value);
-      }
-      else
-      {
-        var newValue = new Dictionary<int, string>
-        {
-          { timestamp, value }
-        };
-        hash.Add(key, newValue);
-      }
+      return string.Empty;
     }
 
-    public string Get(string key, int timestamp)
+    var list = _data[key];
+
+    int left = 0;
+    int right = list.Count - 1;
+
+    while (left < right)
     {
-      string value = string.Empty;
-      if (hash.ContainsKey(key))
+      if (right - left == 1)
       {
-        var existing = hash[key];
-        if (existing.ContainsKey(timestamp)) return existing[timestamp];
-        // right now below line returning TLE exception, we can do better by performing binary search on the dictioanry which holds the timestamp and respected values.
-        var existingValue = existing.Where(item => item.Key <= timestamp).OrderByDescending(item => item.Key).FirstOrDefault();
-        if (existingValue.Equals(default(KeyValuePair<int, string>))) return value;
-        else value = existingValue.Value;
+        break;
       }
 
-      return value;
+      int mid = left + (right - left) / 2;
+
+      var midItem = list[mid];
+      if (midItem.timestamp == timestamp)
+      {
+        return midItem.value;
+      }
+
+      if (midItem.timestamp < timestamp)
+      {
+        left = mid;
+        continue;
+      }
+
+      right = mid;
     }
+
+    if (list[right].timestamp <= timestamp)
+    {
+      return list[right].value;
+    }
+
+    if (list[left].timestamp <= timestamp)
+    {
+      return list[left].value;
+    }
+
+    return string.Empty;
   }
 }
