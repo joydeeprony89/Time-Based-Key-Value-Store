db8f3d8e89c18510cdb9dd41edc36a40f24480cb
joydeeprony89 <joydeeprony89@gmail.com>
2022-06-07 20:15:53 +0530

    Update Program.cs

--- Time Based Key-Value Store/Program.cs
+++ Time Based Key-Value Store/Program.cs
@@ -1,70 +1,42 @@
-﻿using System.Collections.Generic;
-
 public class TimeMap
-{
-  private readonly IDictionary<string, IList<(int timestamp, string value)>> _data;
-
-  public TimeMap()
-  {
-    _data = new Dictionary<string, IList<(int timestamp, string value)>>();
-  }
-
-  public void Set(string key, string value, int timestamp)
-  {
-    if (!_data.ContainsKey(key))
     {
-      _data[key] = new List<(int timestamp, string value)>();
+        private readonly IDictionary<string, IList<(int timestamp, string value)>> _data;
+
+        public TimeMap(){
+            _data = new Dictionary<string, IList<(int timestamp, string value)>>();
+        }
+
+        public void Set(string key, string value, int timestamp){
+            if (!_data.ContainsKey(key)){
+                _data[key] = new List<(int timestamp, string value)>();
+            }
+            _data[key].Add((timestamp, value));
+        }
+
+        public string Get(string key, int timestamp){
+          string res = string.Empty;
+          if(!_data.ContainsKey(key)){
+            return res;
+          }
+          // As the list of values for any key would be in increasing order of time
+          // Why ? because time always increase so when a set would get called for the same key, its ovious that new value for the same key timestamp would be bigger.
+          // as the timestamps are sorted in ASC, we can perform binary search
+          var list = _data[key];
+          int l = 0;
+          int r = list.Count - 1;
+          while(l <= r) {
+            int mid = l+(r-l)/2;
+            // As the question has asked if you find the exact timestamp return it else return the nearest timestamp value
+            if(list[mid].timestamp <= timestamp){
+              // Thats why when a mid element timestamp is less than eq to target timestamp we can update our res
+              // and res will get updated based on BS working
+              res = list[mid].value;
+              l = mid + 1;
+            } else{
+              // As here the mid position timestamp is greater than target so will not update our res.
+              r = mid - 1;
+            }
+          }
+          return res;
+        }
     }
-
-    _data[key].Add((timestamp, value));
-  }
-
-  public string Get(string key, int timestamp)
-  {
-    if (!_data.ContainsKey(key))
-    {
-      return string.Empty;
-    }
-
-    var list = _data[key];
-
-    int left = 0;
-    int right = list.Count - 1;
-
-    while (left < right)
-    {
-      if (right - left == 1)
-      {
-        break;
-      }
-
-      int mid = left + (right - left) / 2;
-
-      var midItem = list[mid];
-      if (midItem.timestamp == timestamp)
-      {
-        return midItem.value;
-      }
-
-      if (midItem.timestamp < timestamp)
-      {
-        left = mid;
-        continue;
-      }
-
-      right = mid;
-    }
-
-    if (list[right].timestamp <= timestamp)
-    {
-      return list[right].value;
-    }
-
-    if (list[left].timestamp <= timestamp)
-    {
-      return list[left].value;
-    }
-
-    return string.Empty;
-  }
-}
